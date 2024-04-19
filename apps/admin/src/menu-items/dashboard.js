a941093f1083b5f6668b6114d1a01273e3cd8777
manishaverma15 <manishaverma15873@gmail.com>
2024-04-19 15:30:57 +0530

    feat: TS-1: admin panel project setup

--- apps/admin/src/menu-items/dashboard.js
+++ apps/admin/src/menu-items/dashboard.js
@@ -1,14 +1,11 @@
-// assets
 import { IconDashboard } from '@tabler/icons-react';
 
-// constant
 const icons = { IconDashboard };
 
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 
 const dashboard = {
   id: 'dashboard',
-  title: 'Dashboard',
   type: 'group',
   children: [
     {
--- apps/admin/src/menu-items/index.js
+++ apps/admin/src/menu-items/index.js
@@ -1,12 +1,7 @@
 import dashboard from './dashboard';
-import pages from './pages';
-// import utilities from './utilities';
-// import other from './other';
-
-// ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages]
+  items: [dashboard]
 };
 
 export default menuItems;

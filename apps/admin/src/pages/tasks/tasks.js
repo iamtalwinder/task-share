e786d8afd644c33c7e630c86bcea6cdb2e8e8956
manishaverma15 <manishaverma15873@gmail.com>
2024-04-25 15:30:36 +0530

    feat: TS-3: add task screen

--- apps/admin/src/pages/tasks/tasks.js
+++ apps/admin/src/pages/tasks/tasks.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   Button,
   Box,
@@ -16,7 +16,6 @@ import {
   Chip
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import styles from './task.module.css';
 import Addtask from 'components/add-task/add-task';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
@@ -25,6 +24,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Popover from '@mui/material/Popover';
 import AddLinkIcon from '@mui/icons-material/AddLink';
 import DownloadIcon from '@mui/icons-material/Download';
+import './tasks.scss';
 
 function createData(title, status) {
   return { title, status };
@@ -39,6 +39,7 @@ const TaskTable = () => {
   const [isAddFormVisible, setIsAddFormVisible] = React.useState(false);
   const [isGenerateLink, setIsGenerateLink] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const navigate = useNavigate();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -51,6 +52,7 @@ const TaskTable = () => {
 
   const handleAddButtonClick = () => {
     setIsAddFormVisible(true);
+    navigate('/add-task')
   };
 
   const handleGenerateLink = () => {
@@ -62,21 +64,20 @@ const TaskTable = () => {
 
   return (
     <Box>
-      <Card sx={{ width: '100%', marginBottom: 3 }}>
+      <Card className='card'>
         <CardContent>
-          <Link to='/add-task' style={{ textDecoration: 'none' }}>
-            <Button variant='outlined' onClick={handleAddButtonClick}>
-              <AddIcon sx={{ marginRight: 1 }} />
-              Add New Task
-            </Button>
-          </Link>
+          <Button variant='outlined' onClick={handleAddButtonClick}
+            className='card__add-new-task'>
+            <AddIcon className='card__add-icon' />
+            Add New Task
+          </Button>
         </CardContent>
       </Card>
 
       {isAddFormVisible && <Addtask />}
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <TableContainer component={Paper} className='table'>
+        <Table aria-label='simple table' className='table__wrapper'>
           <TableHead>
             <TableRow>
               <TableCell align="center">Title</TableCell>
@@ -93,18 +94,24 @@ const TaskTable = () => {
               >
                 <TableCell align="center">{row.title}</TableCell>
                 <TableCell align="center">
-                  <Chip label="React" className={styles.chip} />
+                  <Chip label="React" className='table__chip' />
                   <Chip label="TypeScript" />
                 </TableCell>
                 <TableCell align="center">
-                  <Chip label={row.status} className={styles.chip} />
+                  <Chip label={row.status} className='table__chip' />
                 </TableCell>
                 <TableCell align="center">
-                  <Button className={styles.btn}><EditIcon className={styles.icon} /></Button>
-                  <Button className={styles.btn}><RemoveRedEyeIcon className={styles.icon} /></Button>
-                  <Button className={styles.btn}><DeleteIcon className={styles.icon} /></Button>
-                  <Button className={styles.btn} onClick={handleClick}>
-                    <MoreVertIcon className={styles.icon} />
+                  <Button className='table__action-button'>
+                    <EditIcon />
+                  </Button>
+                  <Button className='table__action-button'>
+                    <RemoveRedEyeIcon />
+                  </Button>
+                  <Button className='table__action-button'>
+                    <DeleteIcon />
+                  </Button>
+                  <Button className='table__action-button' onClick={handleClick}>
+                    <MoreVertIcon />
                   </Button>
                 </TableCell>
                 {(isGenerateLink &&
@@ -118,13 +125,13 @@ const TaskTable = () => {
                       horizontal: 'left'
                     }}
                   >
-                    <Paper sx={{ maxHeight: '100vh', width: '150px', padding: 1, textAlign: 'center' }}>
-                      <Button className={styles.link}>
-                        <AddLinkIcon className={styles.actionIcon} />
+                    <Paper className='paper'>
+                      <Button className='paper__extra-actions'>
+                        <AddLinkIcon className='paper__extra-icon' />
                         <Typography>Generate Link</Typography>
                       </Button>
-                      <Button className={styles.link}>
-                        <DownloadIcon className={styles.actionIcon} />
+                      <Button className='paper__extra-actions'>
+                        <DownloadIcon className='paper__extra-icon' />
                         <Typography>Download</Typography>
                       </Button>
                     </Paper>

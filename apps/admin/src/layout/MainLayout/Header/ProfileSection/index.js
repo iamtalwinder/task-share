575cdbb51956083992213f683d8e2214a27ec0ea
manishaverma15 <manishaverma15873@gmail.com>
2024-06-03 17:35:31 +0530

    feature: TS-16: change user setting dropdowm

--- apps/admin/src/layout/MainLayout/Header/ProfileSection/index.js
+++ apps/admin/src/layout/MainLayout/Header/ProfileSection/index.js
@@ -1,36 +1,25 @@
 import { useState, useRef, useEffect } from 'react';
-
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
-// material-ui
 import { useTheme } from '@mui/material/styles';
 import {
   Box,
   Chip,
   ClickAwayListener,
-  Divider,
   List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
   Paper,
   Popper,
-  Stack,
   Typography
 } from '@mui/material';
 
-// third-party
-import PerfectScrollbar from 'react-perfect-scrollbar';
-
-// project imports
 import MainCard from 'ui-component/cards/MainCard';
 import Transitions from 'ui-component/extended/Transitions';
 
-// assets
 import { IconLogout, IconSettings } from '@tabler/icons-react';
 
-// ==============================|| PROFILE MENU ||============================== //
 
 const ProfileSection = () => {
   const theme = useTheme();
@@ -125,72 +114,52 @@ const ProfileSection = () => {
       >
         {({ TransitionProps }) => (
           <Transitions in={open} {...TransitionProps}>
-            <Paper>
+            <Paper sx={{ padding: 0 }}>
               <ClickAwayListener onClickAway={handleClose}>
                 <MainCard border={false} elevation={16} content={false} boxShadow shadow={theme.shadows[16]}>
-                  <Box sx={{ p: 2 }}>
-                    <Stack>
-                      <Stack direction="row" spacing={0.5} alignItems="center">
-                        <Typography variant="h4">Good Morning,</Typography>
-                        <Typography component="span" variant="h4" sx={{ fontWeight: 400 }}>
-                          Johne Doe
-                        </Typography>
-                      </Stack>
-                      <Typography variant="subtitle2">Project Admin</Typography>
-                    </Stack>
-                    <Divider />
-                  </Box>
-                  <PerfectScrollbar
-                    style={{
-                      height: '100%',
-                      maxHeight: 'calc(100vh - 250px)',
-                      overflowX: 'hidden'
-                    }}
-                  >
-                    <Box sx={{ p: 2 }}>
-                      <List
-                        component="nav"
+                  <Box sx={{ p: 2, padding: 0 }}>
+                    <List
+                      component="nav"
+                      sx={{
+                        width: '100%',
+                        maxWidth: 150,
+                        minWidth: 100,
+                        backgroundColor: theme.palette.background.paper,
+                        borderRadius: '10px',
+                        [theme.breakpoints.down('md')]: {
+                          minWidth: '100%'
+                        },
+                        '& .MuiListItemButton-root': {
+                          mt: 0.5
+                        }
+                      }}
+                    >
+                      <ListItemButton
                         sx={{
-                          width: '100%',
-                          maxWidth: 350,
-                          minWidth: 300,
-                          backgroundColor: theme.palette.background.paper,
-                          borderRadius: '10px',
-                          [theme.breakpoints.down('md')]: {
-                            minWidth: '100%'
-                          },
-                          '& .MuiListItemButton-root': {
-                            mt: 0.5
-                          }
+                          borderRadius: `${customization.borderRadius}px`
                         }}
+                        selected={selectedIndex === 0}
+                        onClick={(event) => handleListItemClick(event, 0, '#')}
                       >
-                        <ListItemButton
-                          sx={{
-                            borderRadius: `${customization.borderRadius}px`
-                          }}
-                          selected={selectedIndex === 0}
-                          onClick={(event) => handleListItemClick(event, 0, '#')}
-                        >
-                          <ListItemIcon>
-                            <IconSettings stroke={1.5} size="1.3rem" />
-                          </ListItemIcon>
-                          <ListItemText primary={<Typography variant="body2">Account Settings</Typography>} />
-                        </ListItemButton>
-                        <ListItemButton
-                          sx={{
-                            borderRadius: `${customization.borderRadius}px`
-                          }}
-                          selected={selectedIndex === 4}
-                          onClick={handleLogout}
-                        >
-                          <ListItemIcon>
-                            <IconLogout stroke={1.5} size="1.3rem" />
-                          </ListItemIcon>
-                          <ListItemText primary={<Typography variant="body2">Logout</Typography>} />
-                        </ListItemButton>
-                      </List>
-                    </Box>
-                  </PerfectScrollbar>
+                        <ListItemIcon>
+                          <IconSettings stroke={1.5} size="1.3rem" />
+                        </ListItemIcon>
+                        <ListItemText primary={<Typography variant="body2">Account Settings</Typography>} />
+                      </ListItemButton>
+                      <ListItemButton
+                        sx={{
+                          borderRadius: `${customization.borderRadius}px`
+                        }}
+                        selected={selectedIndex === 4}
+                        onClick={handleLogout}
+                      >
+                        <ListItemIcon>
+                          <IconLogout stroke={1.5} size="1.3rem" />
+                        </ListItemIcon>
+                        <ListItemText primary={<Typography variant="body2">Logout</Typography>} />
+                      </ListItemButton>
+                    </List>
+                  </Box>
                 </MainCard>
               </ClickAwayListener>
             </Paper>

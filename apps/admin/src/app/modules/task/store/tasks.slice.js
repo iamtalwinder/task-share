1b7c5e64cb892d7509faeb6e5ba2a88b4ceab7fe
manishaverma15 <manishaverma15873@gmail.com>
2024-07-01 14:48:07 +0530

    feature: TS-37: added update task functionality

--- apps/admin/src/app/modules/task/store/tasks.slice.js
+++ apps/admin/src/app/modules/task/store/tasks.slice.js
@@ -5,7 +5,8 @@ import { taskService } from 'app/services';
 const initialState = {
   tasks: [],
   listStatus: APIStatusEnum.IDLE,
-  createTaskStatus: APIStatusEnum.IDLE
+  createTaskStatus: APIStatusEnum.IDLE,
+  updateTaskStatus: APIStatusEnum.IDLE
 };
 
 export const taskSlice = createAppSlice({
@@ -38,9 +39,9 @@ export const taskSlice = createAppSlice({
       }
     ),
     getTask: create.asyncThunk(
-      async (id , { rejectWithValue }) => {
+      async (taskId, { rejectWithValue }) => {
         try {
-          const updatedTask = await taskService.getTaskById(id);
+          const updatedTask = await taskService.getTaskById(taskId);
           return updatedTask;
         } catch (error) {
           return rejectWithValue('Failed to update the task');
@@ -50,13 +51,8 @@ export const taskSlice = createAppSlice({
         pending: (state) => {
           state.createTaskStatus = APIStatusEnum.LOADING;
         },
-        fulfilled: (state, action) => {
+        fulfilled: (state) => {
           state.createTaskStatus = APIStatusEnum.SUCCESS;
-          const index = state.tasks.findIndex((task) => task.id === action.payload.id)
-
-          if (index !== -1) {
-            state.tasks[index] = action.payload;
-          }
         },
         rejected: (state) => {
           state.createTaskStatus = APIStatusEnum.FAILED;
@@ -84,15 +80,44 @@ export const taskSlice = createAppSlice({
           state.listStatus = APIStatusEnum.FAILED;
         }
       }
-    )
+    ),
+
+    updateTask: create.asyncThunk(
+      async ({ taskId, updatedTask }, { rejectWithValue }) => {
+        try {
+          const updated = await taskService.updateTask(taskId, updatedTask);
+          return updated;
+        } catch (error) {
+          return rejectWithValue('Failed to update the task');
+        }
+      },
+      {
+        pending: (state) => {
+          state.updateTaskStatus = APIStatusEnum.LOADING;
+        },
+        fulfilled: (state, action) => {
+          state.updateTaskStatus = APIStatusEnum.SUCCESS;
+          const index = state.tasks.findIndex((task) => task.id === action.payload.id);
+
+          if (index !== -1) {
+            state.tasks[index] = action.payload;
+          }
+        },
+        rejected: (state) => {
+          state.updateTaskStatus = APIStatusEnum.FAILED;
+        }
+      }
+    ),
   }),
   selectors: {
     selectListStatus: (state) => state.listStatus,
-    selectTasks: (state) => state.tasks
+    selectTasks: (state) => state.tasks,
+    selectCreateTaskStatus: (state) => state.createTaskStatus,
+    selectUpdateTaskStatus: (state) => state.updateTaskStatus,
   }
 });
 
-export const { setTasks, getUserTasks, createTask, getTask } = taskSlice.actions;
-export const { selectListStatus, selectTasks } = taskSlice.selectors;
+export const { setTasks, getUserTasks, createTask, getTask, updateTask } = taskSlice.actions;
+export const { selectListStatus, selectTasks, selectCreateTaskStatus, selectUpdateTaskStatus } = taskSlice.selectors;
 
 export default taskSlice.reducer;
--- apps/admin/src/mocks/data/tasks.js
+++ apps/admin/src/mocks/data/tasks.js
@@ -3,6 +3,7 @@ export const userTasks = [
     id: 1,
     title: 'Create todo list 1',
     tags: ['React', 'TypeScript'],
+    description: 'this is the description',
     status: 'used',
     userId: 1
   },
@@ -10,6 +11,7 @@ export const userTasks = [
     id: 2,
     title: 'Create user form using redux 1',
     tags: ['Nextjs', 'ReactJs'],
+    description: 'this is the description',
     status: 'not-used',
     userId: 1
   },
@@ -17,6 +19,7 @@ export const userTasks = [
     id: 3,
     title: 'Create todo list 2',
     tags: ['Nextjs', 'ReactJs'],
+    description: 'this is the description',
     status: 'used',
     userId: 2
   },
@@ -24,6 +27,7 @@ export const userTasks = [
     id: 4,
     title: 'Create user form using redux 2',
     tags: ['TypeScript', 'ReactJs'],
+    description: 'this is the description',
     status: 'not-used',
     userId: 2
   },
@@ -31,6 +35,7 @@ export const userTasks = [
     id: 5,
     title: 'Create todo list 3',
     tags: ['Nextjs', 'ReactJs', 'Material UI'],
+    description: 'this is the description',
     status: 'used',
     userId: 3
   },
@@ -38,6 +43,7 @@ export const userTasks = [
     id: 6,
     title: 'Create user form using redux 3',
     tags: ['Reactjs', 'Material UI'],
+    description: 'this is the description',
     status: 'not-used',
     userId: 3
   }
